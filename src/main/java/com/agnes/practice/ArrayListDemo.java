a50b578c1f0c86cc3b0d2c743864e7333a7e2cc7
AgnesNazie <cheagnesmbo@gmail.com>
2025-04-14 11:53:08 +0200

    Completed  exercise 5 & 6

--- src/main/java/com/agnes/practice/ArrayListDemo.java
+++ src/main/java/com/agnes/practice/ArrayListDemo.java
@@ -2,6 +2,8 @@ package com.agnes.practice;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class ArrayListDemo {
     public static void main(String[] args) {
@@ -9,6 +11,8 @@ public class ArrayListDemo {
         exe2();
         exe3();
         exe4();
+        exe5();
+        exe6();
 
     }
 
@@ -40,10 +44,30 @@ public class ArrayListDemo {
 
     public static void exe4() {
         ArrayList<String> week = new ArrayList<>(Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"));
-        System.out.println("=============================EXERCISE4===========================");
+        System.out.println("=============================EXERCISE 4===========================");
+        System.out.println("ORIGINAL LIST");
         System.out.println(week);
-        ArrayList<String> firstThree = new ArrayList<> (week.subList(0, 3));
+        ArrayList<String> firstThree = new ArrayList<>(week.subList(0, 3));
+        System.out.println("SUBLIST");
         System.out.println(firstThree);
+
+    }
+
+    public static void exe5() {
+        HashSet<String> days = new LinkedHashSet<>(Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"));
+        System.out.println("=============================EXERCISE 5===========================");
+        System.out.println(days);
+
+    }
+
+    public static void exe6() {
+        HashSet<String> days = new LinkedHashSet<>(Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"));
+        System.out.println("=============================EXERCISE 6===========================");
+        System.out.println("ORIGINAL HASHSET");
+        System.out.println(days);
+        ArrayList<String> dayList = new ArrayList<>(days);
+        System.out.println("Convert Hashset to List");
+        System.out.println(dayList);
 
     }
 }
